d3b29ba2e6a4ab18afd4a10eb52bc8940254c8ea
Pierattilio-correale <pierattiliocorreale@yahoo.it>
2025-08-06 02:54:01 +0200

    starting home page

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,25 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import Sidebar from "./components/Sidebar";
+import MyForm from "./components/MyForm";
+import { Col, Container, Row } from "react-bootstrap";
 
 function App() {
   return (
     <>
-      <Sidebar />
+      {" "}
+      <Container className="m-0 p-0">
+        <Row>
+          <Col>
+            {" "}
+            <Sidebar />
+          </Col>
+          <Col>
+            {" "}
+            <MyForm />
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 }
---
+++ src/components/MyForm.tsx
@@ -0,0 +1,10 @@
+import { Col, Container, Row, Form, InputGroup } from "react-bootstrap";
+import { FaSearch } from "react-icons/fa";
+const MyForm = () => {
+  return (
+    <>
+      <Col></Col>
+    </>
+  );
+};
+export default MyForm;
